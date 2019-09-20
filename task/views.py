79efd732715c97d510e068907a0a367a6d265cbf
tiago <tfmels2@gmail.com>
2019-09-20 02:28:30 -0300

    outro commit

--- task/views.py
+++ task/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-
+from .models import task
 # Create your views here.
 
 def task_list(request):
-    return render(request, 'task/task_list.html', {})
+    tasks = task.objects.order_by('datedeadline')
+    return render(request, 'task/task_list.html', {'tasks': tasks})
